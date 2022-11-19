691191cba0ad891cd2f811a1f2e038976e7a6c46
Amy Detwiler <detwiler.amy@gmail.com>
2022-11-18 21:14:13 -0500

    added two stubs to the stubs folder

---
+++ stubs/imagesStub.js
@@ -0,0 +1,66 @@
+export default {
+  data: {
+    images: [
+      {
+        src: 'https://i.seadn.io/gae/pUOKgIQoZoDMbd7FE9pjpa2ZxtljJv88W6IAVOLFV6MAe9Osw3QzWEHepLP5FYX8HqnvJQx_fnIxk1cdnjkcbH9l0CHc83A4ZGC8fA',
+        name: 'Fauvism Banks 2022 BannedBooksArt X KIKU’S WORLD by SABET',
+        permaLink: 'https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/24022912508817508199319788437121766259588087588617902734718768192536761597953',
+        assetContractAddress: ''
+      },
+      {
+        src: 'https://i.seadn.io/gae/PFXwUXsphB8Ai3RlwKuMW99lrHnROjSxOwRu49PXNEb14NGEgFA-pwSaz3Dsf8yr_imV1y_Q4J8Ccn_nwrPn2e9aYCaQrmEfg9uHLA',
+        name: 'Bigfoot Shreds',
+        permaLink: 'https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/24022912508817508199319788437121766259588087588617902734718768183740668575745',
+        assetContractAddress: ''
+      },
+      {
+        src: 'https://i.seadn.io/gae/1_Zsgu4rkIV-veKDQaIDoeqgsujX1XGXCozOTkM74cwTktKeqFCwfvB4ZCyHOMe6VYl0554gvLqm3LLc28v1X-sxfpIWdMA7pcn2eQ',
+        name: 'The Rugs for Better Lives #12!',
+        permaLink: 'https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/24022912508817508199319788437121766259588087588617902734718768187039203459078',
+        assetContractAddress: ''
+      },
+      {
+        src: 'https://i.seadn.io/gae/oGFzLdLzKqcwZ6BB562jTtRlnAX56Ht6bQo1xNfVftBCQBjM2JvvYxpkiz7uOOtekfYgDDN5fbASqQQ5xxcAsZkt3nJRrmGl0fza',
+        name: 'The Rugs for Better Lives 80!',
+        permaLink: 'https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/24022912508817508199319788437121766259588087588617902734718768242014784847873',
+        assetContractAddress: ''
+      },
+      {
+        src: 'https://i.seadn.io/gae/ktNbhmGSGk_YSHEdu_ji6FyTDtyJy40dSyEq5UKHLpJqLS8rLSZpAo6zqimLcSqYWhlWKZynHq5tmgO2Ibk3B8a6srR7KMHFH6-fBQ',
+        name: 'The Rugs for Better Lives #67!',
+        permaLink: 'https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/24022912508817508199319788437121766259588087588617902734718768233218691825665',
+        assetContractAddress: ''
+      },
+      {
+        src: 'https://i.seadn.io/gae/tm3ygDSe0kTcrsHSSQ9tcHWTLY7ZxnZHEqIojuC8QdoVy4YC07Fu8LvDP1x2remEcrWG5w4_M4bA1GaSeBF6ymcFFoGR7GWTFjqiRBM',
+        name: 'Grimm BannedBooks Charity Collect #16',
+        permaLink: 'https://opensea.io/assets/ethereum/0x5a88d2562ac55ea2cd1cab1fd730070536ec5cd2/0',
+        assetContractAddress: ''
+      },
+      {
+        src: 'https://i.seadn.io/gae/tqSUHLl1Smkl09gMe_8vvabd-ONS1d2ndN6IakCQ2DEzx6x8tFUsYU2jrL-2_Id2Ox4-owXX7gFCVn8I6Mp_VquJCV3_6fiFOjx0',
+        name: 'Grimm Art',
+        permaLink: 'https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/94367166023689824508121337607089910239332488566642841689732307872206362247169',
+        assetContractAddress: ''
+      },
+      {
+        src: 'https://i.seadn.io/gae/eGXxECyZc6TxTDeCCfMQMqWwQX0rG0gT9tQ72NEr1AkEChAS-yipeyOa-yEfktzTuQYn8GwEzndsQfgZxw5hP6CZztVCUWQeQs4iqWs',
+        name: 'BannedBooks Beverage',
+        permaLink: 'https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/94367166023689824508121337607089910239332488566642841689732307794141036675073',
+        assetContractAddress: ''
+      },
+      {
+        src: 'https://i.seadn.io/gae/B6lTsvVCZTI3Y9dx8VziUzfVa7qzUAfB-uJ3CqxQLscHf0UIuyKU-3zcrI5Jzkbk895L3XlvR6RbiXXtkjecKCMv4b_d31XUK_Jz?auto=format&w=1000',
+        name: 'I am Pony Series 1 T-Shirt',
+        permaLink: 'https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/82792700510954771777686919428563069321607068200305279657934273197947960164372',
+        assetContractAddress: ''
+      },
+      {
+        src: 'https://i.seadn.io/gae/r6fHfifh3WnI5MTdHCgcxHsithIoFxxwV0rblazaGIHyaRPmI2hwbc5DmmNYIkDnpzHqii_7p9DHKfJfjAT2P3jmSsXJGIT9MiDk1Q?auto=format&w=1000',
+        name: 'GreggyB',
+        permaLink: 'https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/82792700510954771777686919428563069321607068200305279657934273184753820631041',
+        assetContractAddress: ''
+      },
+    ]
+  }
+}
---
+++ stubs/openSeaAssetsStub.js
@@ -0,0 +1,222 @@
+{
+  "next": "LXBrPTI5OTE2MDIxOQ%3D%3D",
+  "previous": null,
+  "assets": [
+    {
+      "id": 299160222,
+      "num_sales": 0,
+      "background_color": null,
+      "image_url": "https://lh3.googleusercontent.com/OIUpIvOXhqG8F-Wa948rxJcA8bg5lQGGaJ2yquegulYrD3nncituY5DcOi_wnpVumHU4qEBR-G-zBRNsQusbIgETVcTadt0r5arh32A",
+      "image_preview_url": "https://lh3.googleusercontent.com/OIUpIvOXhqG8F-Wa948rxJcA8bg5lQGGaJ2yquegulYrD3nncituY5DcOi_wnpVumHU4qEBR-G-zBRNsQusbIgETVcTadt0r5arh32A=s250",
+      "image_thumbnail_url": "https://lh3.googleusercontent.com/OIUpIvOXhqG8F-Wa948rxJcA8bg5lQGGaJ2yquegulYrD3nncituY5DcOi_wnpVumHU4qEBR-G-zBRNsQusbIgETVcTadt0r5arh32A=s128",
+      "image_original_url": null,
+      "animation_url": null,
+      "animation_original_url": null,
+      "name": "Planet 0310-MI-RII",
+      "description": null,
+      "external_link": null,
+      "asset_contract": {
+        "address": "0x495f947276749ce646f68ac8c248420045cb7b5e",
+        "asset_contract_type": "semi-fungible",
+        "created_date": "2020-12-02T17:40:53.232025",
+        "name": "OpenSea Collection",
+        "nft_version": null,
+        "opensea_version": "2.0.0",
+        "owner": 102384,
+        "schema_name": "ERC1155",
+        "symbol": "OPENSTORE",
+        "total_supply": null,
+        "description": "",
+        "external_link": null,
+        "image_url": null,
+        "default_to_fiat": false,
+        "dev_buyer_fee_basis_points": 0,
+        "dev_seller_fee_basis_points": 0,
+        "only_proxied_transfers": false,
+        "opensea_buyer_fee_basis_points": 0,
+        "opensea_seller_fee_basis_points": 250,
+        "buyer_fee_basis_points": 0,
+        "seller_fee_basis_points": 250,
+        "payout_address": null
+      },
+      "permalink": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/101577219900953729483682340185834053539485491429073128558041914785105988026369",
+      "collection": {
+        "banner_image_url": null,
+        "chat_url": null,
+        "created_date": "2022-02-16T06:41:53.565898",
+        "default_to_fiat": false,
+        "description": "We were born in a diverse, ever changing, spectacular universe which keeps, even after millions of years, feeding our curiosity to uncover its mysteries and reach farther hidden places. This collection reflects the embedded diversity in the universe, and in ourselves, and provides each of us the opportunity to represent their unique personality.   \nExplore a collection of 6000 unique and algorithmically created colorful planets. Find still and moving planets, each with its own unique geography, moons, rings, and orbits. Their counts, colors and arrangement make for different complexities and provide some aesthetically pleasing scenery. Hurry up and reserve your place in space now.  \n\n## How to Navigate\n- Filter by \"Planet 01\" to \"Planet 20\" to find planets of 20 different styles\n- Filter by \"Planet XX01\" to \"Planet XX07\" to find still planets, and \"Planet XX08\" to \"Planet \"XX10\" to find moving ones",
+        "dev_buyer_fee_basis_points": "0",
+        "dev_seller_fee_basis_points": "1000",
+        "discord_url": null,
+        "display_data": {
+          "card_display_style": "cover"
+        },
+        "external_url": null,
+        "featured": false,
+        "featured_image_url": null,
+        "hidden": false,
+        "safelist_request_status": "not_requested",
+        "image_url": "https://lh3.googleusercontent.com/IPf5tBHMv7dCDlOTJFeZbIi3vL4ONikq359W-fASBDxBwrwu6qfzfwqTMqh8fLs7wNueMurvSd77lsia7WZ3PH10YENdmDW5e1A1VA=s120",
+        "is_subject_to_whitelist": false,
+        "large_image_url": null,
+        "medium_username": null,
+        "name": "Planets of a Digital World",
+        "only_proxied_transfers": false,
+        "opensea_buyer_fee_basis_points": "0",
+        "opensea_seller_fee_basis_points": "250",
+        "payout_address": "0xe092ab4843725299968ed46ec6a6ae6c3232428e",
+        "require_email": false,
+        "short_description": null,
+        "slug": "planets-digital-world",
+        "telegram_url": null,
+        "twitter_username": null,
+        "instagram_username": null,
+        "wiki_url": null,
+        "fees": {
+          "opensea_fees": {
+            "0x0000a26b00c1f0df003000390027140000faa719": 250
+          },
+          "seller_fees": {
+            "0xe092ab4843725299968ed46ec6a6ae6c3232428e": 1000
+          }
+        }
+      },
+      "decimals": null,
+      "token_metadata": null,
+      "owner": {
+        "user": {
+          "username": "NullAddress"
+        },
+        "profile_img_url": "https://storage.googleapis.com/opensea-static/opensea-profile/1.png",
+        "address": "0x0000000000000000000000000000000000000000",
+        "config": ""
+      },
+      "sell_orders": null,
+      "creator": {
+        "user": {
+          "username": "AItisticEngineer"
+        },
+        "profile_img_url": "https://storage.googleapis.com/opensea-static/opensea-profile/3.png",
+        "address": "0xe092ab4843725299968ed46ec6a6ae6c3232428e",
+        "config": ""
+      },
+      "traits": [],
+      "last_sale": null,
+      "top_bid": null,
+      "listing_date": null,
+      "is_presale": true,
+      "transfer_fee_payment_token": null,
+      "transfer_fee": null,
+      "token_id": "101577219900953729483682340185834053539485491429073128558041914785105988026369"
+    },
+    {
+      "id": 299160219,
+      "num_sales": 0,
+      "background_color": null,
+      "image_url": "https://lh3.googleusercontent.com/xbwAomX-LNegpzC7nfK9ACDA6GlmAJuMYHIP0X3briF6Md70nkSdDEoEUyQzEk_T5f56iSjqSvcRTmIQCP-fOfgCuZDzFnGWSPKeHQ",
+      "image_preview_url": "https://lh3.googleusercontent.com/xbwAomX-LNegpzC7nfK9ACDA6GlmAJuMYHIP0X3briF6Md70nkSdDEoEUyQzEk_T5f56iSjqSvcRTmIQCP-fOfgCuZDzFnGWSPKeHQ=s250",
+      "image_thumbnail_url": "https://lh3.googleusercontent.com/xbwAomX-LNegpzC7nfK9ACDA6GlmAJuMYHIP0X3briF6Md70nkSdDEoEUyQzEk_T5f56iSjqSvcRTmIQCP-fOfgCuZDzFnGWSPKeHQ=s128",
+      "image_original_url": null,
+      "animation_url": null,
+      "animation_original_url": null,
+      "name": "full snow moon 09",
+      "description": "My attempt to capture the majestic feminine of a full moon through colour and movement in my ‘arty’ abstractionism using a layering of digital techniques.",
+      "external_link": null,
+      "asset_contract": {
+        "address": "0x495f947276749ce646f68ac8c248420045cb7b5e",
+        "asset_contract_type": "semi-fungible",
+        "created_date": "2020-12-02T17:40:53.232025",
+        "name": "OpenSea Collection",
+        "nft_version": null,
+        "opensea_version": "2.0.0",
+        "owner": 102384,
+        "schema_name": "ERC1155",
+        "symbol": "OPENSTORE",
+        "total_supply": null,
+        "description": "",
+        "external_link": null,
+        "image_url": null,
+        "default_to_fiat": false,
+        "dev_buyer_fee_basis_points": 0,
+        "dev_seller_fee_basis_points": 0,
+        "only_proxied_transfers": false,
+        "opensea_buyer_fee_basis_points": 0,
+        "opensea_seller_fee_basis_points": 250,
+        "buyer_fee_basis_points": 0,
+        "seller_fee_basis_points": 250,
+        "payout_address": null
+      },
+      "permalink": "https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/89251628251019127135783150474798842902821279268347685136567997309972058734593",
+      "collection": {
+        "banner_image_url": "https://lh3.googleusercontent.com/gLs5zFl3vVTOm3eqQl63z_kEenZNsbeaYsJd9oxWDmlzU2AQ_mL_6Jh-Y1cw5dPKw2KzjdSxwgY3O05B0RCdKFR_aCkxLO1yt4fHLg=s2500",
+        "chat_url": null,
+        "created_date": "2022-02-18T19:57:41.429402",
+        "default_to_fiat": false,
+        "description": "The full moon on feb 16th (snow moon) inspired a series of abstracts. The intention was to capture her majestic femininity through colour and movement. ",
+        "dev_buyer_fee_basis_points": "0",
+        "dev_seller_fee_basis_points": "1000",
+        "discord_url": null,
+        "display_data": {
+          "card_display_style": "cover"
+        },
+        "external_url": null,
+        "featured": false,
+        "featured_image_url": "https://lh3.googleusercontent.com/o-de5j3CKewllk3TAzhsWMDal0Wpj-akv2GP9V1HSO2Sl0dfG4w88CYVi62GVUBJu9CGMR2ogk4T8B6am3B3I8osCBpWdr0ZAlA20J4=s300",
+        "hidden": false,
+        "safelist_request_status": "not_requested",
+        "image_url": "https://lh3.googleusercontent.com/hG1ua2qNPkh6eFfFgCzaWhzidU-d0J4BdrjpXKvk5ZPn9qQUO-18YU2kICjkcbKMYtYklXKPII8vOgVPbOc0-HC8eDWPf7yq6bfp_rs=s120",
+        "is_subject_to_whitelist": false,
+        "large_image_url": "https://lh3.googleusercontent.com/o-de5j3CKewllk3TAzhsWMDal0Wpj-akv2GP9V1HSO2Sl0dfG4w88CYVi62GVUBJu9CGMR2ogk4T8B6am3B3I8osCBpWdr0ZAlA20J4=s300",
+        "medium_username": null,
+        "name": "Snow Moon Abstracts",
+        "only_proxied_transfers": false,
+        "opensea_buyer_fee_basis_points": "0",
+        "opensea_seller_fee_basis_points": "250",
+        "payout_address": "0xc552a1a4acebd7d9aafde9e880ef0026b3ba1d71",
+        "require_email": false,
+        "short_description": null,
+        "slug": "snow-moon-abstracts",
+        "telegram_url": null,
+        "twitter_username": null,
+        "instagram_username": "yourart",
+        "wiki_url": null,
+        "fees": {
+          "opensea_fees": {
+            "0x0000a26b00c1f0df003000390027140000faa719": 250
+          },
+          "seller_fees": {
+            "0xc552a1a4acebd7d9aafde9e880ef0026b3ba1d71": 1000
+          }
+        }
+      },
+      "decimals": null,
+      "token_metadata": null,
+      "owner": {
+        "user": {
+          "username": "NullAddress"
+        },
+        "profile_img_url": "https://storage.googleapis.com/opensea-static/opensea-profile/1.png",
+        "address": "0x0000000000000000000000000000000000000000",
+        "config": ""
+      },
+      "sell_orders": null,
+      "creator": {
+        "user": {
+          "username": "yourartylife"
+        },
+        "profile_img_url": "https://storage.googleapis.com/opensea-static/opensea-profile/31.png",
+        "address": "0xc552a1a4acebd7d9aafde9e880ef0026b3ba1d71",
+        "config": ""
+      },
+      "traits": [],
+      "last_sale": null,
+      "top_bid": null,
+      "listing_date": null,
+      "is_presale": true,
+      "transfer_fee_payment_token": null,
+      "transfer_fee": null,
+      "token_id": "89251628251019127135783150474798842902821279268347685136567997309972058734593"
+    }
+  ]
+}
