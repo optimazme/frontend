0f884f80b9709e11362133dd99173a2705e09ff8
Amy Detwiler <detwiler.amy@gmail.com>
2022-11-24 00:59:29 -0500

    added words to the images

--- components/mazes/SideBar.vue
+++ components/mazes/SideBar.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>
+  <div class="h-full">
     <h1>Metamaze</h1>
     <div>
       {{foundImagesLength}} / {{showImagesLength}}
     </div>
-    <div class="h-60 w-full overflow-y-auto">
-      <div v-for="(image, index) in foundImages" :key="`side-image-${index}`" class="w-40">
+    <div class="h-96 w-full overflow-y-auto">
+      <div v-for="(image, index) in foundImages" :key="`side-image-${index}`" class="w-60 flex flex-col md:flex-row">
         <NuxtImg class="w-20" :src="image.src" />
+        <span>{{image.word}}</span>
       </div>
+    </div>
+    <button @click="endGame">End Game</button>
+    <div v-if="foundPhrases">
+      {{aiPhrase}}
     </div>
   </div>
 </template>
@@ -15,9 +20,40 @@
 import Vue from 'vue'
 import { mapGetters } from 'vuex'
 
-export default Vue.extend({
+interface Data {
+
+}
+
+interface Methods {
+
+}
+
+interface Components {
+
+}
+
+interface Props {
+
+}
+
+interface Computed {
+  foundPhrases: string[]
+}
+
+export default Vue.extend<Data, Methods, Components, Props, Computed>({
   computed: {
-    ...mapGetters('maze', ['foundImages', 'foundImagesLength', 'showImagesLength'])
+    ...mapGetters('maze', ['foundImages', 'foundImagesLength', 'foundPhrases', 'showImagesLength']),
+    aiPhrase() {
+      return this.foundPhrases.join(',')
+    }
+    
+  },
+  methods: {
+    endGame() {
+      const foundPhrases = this.$store.getters['maze/foundImages'].map((x: any) => { return x.word })
+      console.log({ foundPhrases })
+      this.$store.dispatch('maze/setFoundPhrases', foundPhrases)
+    }
   }
 })
 </script>
